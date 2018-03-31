164df503e6810bbd693fdead8c3aed04b52ded84
Seth <itz.saga@gmail.com>
2018-03-30 20:46:17 -0500

    Added about link test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list-rentals', function(hooks) {
@@ -11,6 +11,9 @@ module('Acceptance | list-rentals', function(hooks) {
   });
 
   test('should link to information about the company', async assert => {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information', async assert => {
