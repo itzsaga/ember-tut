20df5f75e9406e2205aabd720b0915e87f778b42
Seth <itz.saga@gmail.com>
2018-04-03 20:35:42 -0500

    Add input to filter list

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['list-filter'],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    this.get('filter')('').then(allResults => {
+      this.set('results', allResults.results);
+    });
+  },
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+      filterAction(filterInputValue).then(filterResults => {
+        if (filteredResults.query === this.get('value')) {
+          this.set('results', filterResults.results);
+        }
+      });
+    }
+  }
 });
