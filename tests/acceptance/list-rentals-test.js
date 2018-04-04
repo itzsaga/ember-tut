2d55d706f6769febd68eb2506bf3d1ccbe26db2c
Seth <itz.saga@gmail.com>
2018-04-03 21:11:37 -0500

    Add acceptance test test for filter

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click, currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, visit, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -30,6 +30,11 @@ module('Acceptance | list-rentals', function(hooks) {
   });
 
   test('should filter the list of rentals by city', async assert => {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.ok(this.element.querySelector('.results .listing'), 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 
   test('should show details for a selected rental', async assert => {
